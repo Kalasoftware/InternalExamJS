a532d9ac6caab9d535c534dcc270bd0a51c2b1a9
kalasoftware <newsoftwarewala@gmail.com>
2024-03-23 23:20:10 +0530

    some more changes are needed

---
+++ index.js
@@ -0,0 +1,157 @@
+
+function namecheck(name,err){ // space is valid in this expersion 
+    if(!/^[A-Za-z\s]{2,10}/.test(name.value)){
+        err.style.color="red"; // remove the s for no space 
+            err.innerHTML = "Error in name";
+            return false;
+    }
+    else 
+    {
+        err.style.color="green"
+        err.innerHTML="valid"
+        return true;
+    }
+}
+
+function mobilecheck(mobile,err)
+{
+ if(!/^[789]{3}[0-9]{7}$/.test(mobile.value.trim())){
+        err.style.color="red"; // remove the s for no space 
+            err.innerHTML = "Error in mobile number";
+            return false;
+    }
+    else 
+    {
+        err.style.color="green"
+        err.innerHTML="valid"
+        return true;
+    }
+}
+
+function emailchecK(email,err){
+     if(!/^[a-zA-Z_0-9-]+@[a-zA-Z0-9-_]+\.[a-z]{2,4}$/.test(email.value)){
+        err.style.color="red"; // remove the s for no space 
+            err.innerHTML = "Please Enter valid mail Address";
+            return false;
+    }
+    else 
+    {
+        err.style.color="green"
+        err.innerHTML="valid"
+        return true;
+    }
+}
+
+function urlvalid(linked,err){
+
+    if(!/^(https?:\/\/)?(in\.)?linkedin\.com\//.test(linked.value))
+    {
+        err.style.color="red"
+        err.innerHTML="Please enter valid link";
+        return false;
+    }
+    else 
+    {
+        err.style.color="green"
+        err.innerHTML = "valid link"
+        return true;
+    }
+
+}
+
+function comboBox(ownbox,err)
+{
+    if(ownbox.selectedIndex==0){
+        err.style.color="red"
+        err.innerHTML = "please select the value"
+    }else 
+    {
+        err.style.color="green"
+        err.innerHTML = "valid";
+        var ans = ownbox.value
+        console.log(ans)
+    }
+}
+// created dynamical radio button
+var radiocont = document.getElementById("radiocontiner")
+dynamicradio(radiocont)
+function dynamicradio(radiocont)
+{
+    var radioValues = ["10th","12th","Diploma","School"]
+
+    for(var i =0; i<radioValues.length; i++)
+    {
+        var radiotbn = document.createElement("input")
+        radiotbn.type="radio"
+        radiotbn.name="education"
+        radiotbn.value=radioValues[i]
+
+        var label = document.createElement("label")
+        label.textContent = radioValues[i]
+
+        // now appending it into the container 
+
+        radiocont.appendChild(radiotbn)
+        radiocont.appendChild(label)
+        radiocont.appendChild(document.createElement("br"))
+    }
+}
+// validating the radio button
+function validRadio(err)
+{
+    var radioele = document.getElementsByName("education")
+    var radioC = false;
+    for(i=0; i<radioele.length; i++)
+    {
+        if(radioele[i].checked)
+        {
+            var radioValue = radioele[i];
+            radioC=true;
+            break;
+        }
+    }
+    if(radioC)
+    {
+        err.style.color="green"
+        err.innerHTML = "valid"
+        return true;
+    }else 
+    {
+        err.style.color="red"
+        err.innerHTML = "please check atleast one radio button"
+        return false;
+    }
+}
+// storing the data 
+function storeFormData(fname, lname, mobile, email, linkdin, quali,radiovalue) {
+    let myFormdata = {
+        firstname: fname,
+        lastname: lname,
+        mobile: mobile,
+        email: email,
+        linkdin: linkdin,
+        quali: quali,
+        radiovalue : radiovalue
+    };
+    localStorage.setItem('myFormData', JSON.stringify(myFormdata));
+    console.log("stored data")
+}
+// getting the data 
+function showData(divele){
+
+    var storedData = localStorage.getItem('myFormData')
+    if(storedData){
+        var formData = JSON.parse(storedData)
+        divele.innerHTML = `<h4> Stored data</h4>
+                            First Name = ${formData.firstname} <br>
+                            Last Name = ${formData.lastname}<br>
+                            Mobile = ${formData.mobile}<br>
+                            email = ${formData.email}<br>
+                            linkdin link = ${formData.linkdin}<br>
+                            qualification = ${formData.quali}<br>
+                            Select Radio value = ${formData.radiovalue}
+                            <br>
+        `
+    }
+
+}
